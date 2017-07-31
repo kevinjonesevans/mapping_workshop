d8eb14d390624a74abe7119255e3bd3ea02e8d2f
Kevin Jones-Evans <jonesevans.kevin.t@gmail.com>
2017-07-31 08:46:46 -0400

    Adds changes from the mapping workshop day

--- map.js
+++ map.js
@@ -9,13 +9,36 @@ $(document).ready(function() {
     // center the map at (Dayton), and what zoom level to use.
     var mymap = L.map('map').setView(DAYTON, 6);
 
-    // This is our base tile layer for our map. We'll talk about this more in
-    // the next lesson.
-    var tiles = L.tileLayer('http://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png', {
-        maxZoom: 18,
+    L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
         attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-    });
+    }).addTo(mymap);
 
-    // Add a layer to the map
-    mymap.addLayer(tiles);
+    var countiesLayer = L.featureGroup();
+    countiesLayer.addTo(mymap);
+
+    var bindPopup = function(feature, layer) {
+        var countyTemplate = '<h2>{COUNTY_NAM}</h2><p>FIPS Code: {FIPS_CODE}</p><p>2000 Population: {POP_2000}</p>';
+        layer.bindPopup(L.Util.template(countyTemplate, feature.properties));
+    };
+
+    var color = function(feature){
+      var num = feature.properties.ELEVATION_/255.0;
+      return {color: `rgb(${num},${num},${num})`};
+    }
+
+    // First, we need to load the GeoJSON file
+    $.get('counties.oh.json', function(data) {
+
+        // Since Leaflet already knows how to render GeoJSON, we won't have to
+        // do any processing to the data.
+        L.geoJson(data, {
+            style: color,
+            onEachFeature: bindPopup
+        }).addTo(countiesLayer);
+
+        // We can still zoom to the extents of a layer by using our old
+        // FeatureGroup layer trick
+        mymap.fitBounds(countiesLayer.getBounds());
+    }); // $.get()
 });
